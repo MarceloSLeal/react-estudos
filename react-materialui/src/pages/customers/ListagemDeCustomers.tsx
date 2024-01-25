83192dd086c29cc5dc3f644a2227b067ef99a931
MarceloSLeal <marcelosleal@outlook.com>
2024-01-24 21:18:51 -0300

    Iniciando a requisição na API e retornou os dados como esperado, agora preciso fazer mais alguns ajustes e montar a lista na tela

--- react-materialui/src/pages/customers/ListagemDeCustomers.tsx
+++ react-materialui/src/pages/customers/ListagemDeCustomers.tsx
@@ -1,8 +1,9 @@
-import React, { useMemo } from "react";
-import { LayoutBaseDePagina } from "../../shared/layouts";
-import { FerramentasDaListagem } from "../../shared/components";
-
+import React, { useEffect, useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
+
+import { CustomersService } from "../../shared/services/api/customers/CustomersService";
+import { FerramentasDaListagem } from "../../shared/components";
+import { LayoutBaseDePagina } from "../../shared/layouts";
 
 
 export const ListagemDeCustomers: React.FC = () => {
@@ -12,18 +13,29 @@ export const ListagemDeCustomers: React.FC = () => {
         return searchParams.get("busca") || "";
     }, [searchParams]);
 
+    useEffect(() => {
+
+        CustomersService.buscar()
+        .then((result) => {
+            if (result instanceof Error) {
+                alert(result.message);
+            } else {
+                console.log(result);
+            }
+        });
+    }, []);
+
     return (
         <LayoutBaseDePagina 
             titulo="Listagem de customers"
             barraDeFerramentas={
-                <FerramentasDaListagem
-                    mostrarInputBusca
-                    textoDaBusca={busca}
-                    textoBotaoNovo="Novo"
-                    aoMudarTextoDeBusca={ texto => setSearchParams({ busca: texto}, { replace: true })}
-                />
-            }
-        >
+            <FerramentasDaListagem
+                mostrarInputBusca
+                textoDaBusca={busca}
+                textoBotaoNovo="Novo"
+                aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })} />}
+                children={undefined}
+            >
 
         </LayoutBaseDePagina>
     );
