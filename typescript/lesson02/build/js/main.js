1656590298827d18bed42e44ac6cd39bf620a074
MarceloSLeal <marcelosleal@outlook.com>
2024-01-29 17:22:35 -0300

    Lesson02 alguns tipo de dados, declaração implícita e explícita

---
+++ typescript/lesson02/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName = 'Marcelo';
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'Marcelo';
+meaningOfLife = 42;
+isLoading = true;
+album = 1979;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
