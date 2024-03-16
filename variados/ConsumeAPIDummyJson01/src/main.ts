f41b3dad8791e909fec2a1a5f85fc358013a4159
MarceloSLeal <marcelosleal@outlook.com>
2024-03-16 04:37:34 -0300

    salvando

--- variados/ConsumeAPIDummyJson01/src/main.ts
+++ variados/ConsumeAPIDummyJson01/src/main.ts
@@ -1,27 +1,75 @@
-import ListItem from "./model/ListItem";
+// import ListItem from "./model/ListItem";
+import axios from "axios";
 
 const initApp = (): void => {
 
-    interface Prod {
+    interface Products {
         id: number,
         title: string,
+        description: string,
+        price: number,
     }
+
+    let productsInstance: Products[] = [];
 
     const url = "https://dummyjson.com/products";
-    const fetchProducts = async (): Promise<Prod[]> => {
-        
-        const data = await fetch(url)
-        .then(res => {
-                return res.json()
-            }).catch(err => {
-                if (err instanceof Error) console.log(err.message);
-            })
-        return data;
+
+    // async function getProductsFromAPI(): Promise<Products[]> {
+    //     try {
+    //         const response = await axios.get(url);
+    //         const productsAPI: Products[] = response.data;
+    //         return productsAPI;
+    //     } catch (error) {
+    //         console.error("Error to get API " + error);
+    //         return [];
+    //     }
+    // }
+
+    // async function main(): Promise<Products[]> {
+    //     productsInstance = await getProductsFromAPI();
+    //     console.log(productsInstance.length);
+    //     return productsInstance;
+    // }
+
+    // console.log(main());
+
+
+    ///////////////////
+
+
+    interface Todo {
+        userId: number,
+        id: number,
+        title: string,
+        completed: boolean,
     }
 
-    type FetchProductsReturnType = Awaited<ReturnType<typeof fetchProducts>>;
+    let todoInstance: Todo[] = [];
 
-    fetchProducts().then(prod => console.log(prod));
+    async function fetchToDo<T>(resourceUrl: string): Promise<T[]> {
+        return fetch(resourceUrl).then(response => {
+            return response.json();
+        })
+    }
+
+    fetchToDo<Todo>("https://jsonplaceholder.typicode.com/todos")
+        .then((toDoItem) => {
+            // let myNewToDo: Todo[] = toDoItem
+            // console.log(toDoItem);
+            console.log(toDoItem.length);
+            console.log(toDoItem[2]);
+            
+            for(let i = 0; i > toDoItem.length; i++) {
+                todoInstance[i].userId = toDoItem[i].userId;
+            }
+        });
+
+    todoInstance.forEach((e) => {
+        console.log(e);
+    })
+
+    console.log(todoInstance.length);
+
 
 
 }
