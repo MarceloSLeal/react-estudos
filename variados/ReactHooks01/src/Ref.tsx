6a5bcce7c2072cea67f77859037e03ba68534d62
MarceloSLeal <marcelosleal@outlook.com>
2024-04-07 17:00:52 -0300

    Exemplo de useRef, forma de acessar e referenciar objetos de DOM e suas funções

--- variados/ReactHooks01/src/Ref.tsx
+++ variados/ReactHooks01/src/Ref.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const RefTutorial = () => {
+
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    const setFocus = () => {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    }
+    const setClear = () => {
+        if (inputRef.current) {
+            inputRef.current.value = "";
+        }
+    }
 
     return (
         <div className="div2">
             <h2>---useRef---</h2>
+            <input type="text" placeholder="Ex..." ref={inputRef}/>
+            <button onClick={setFocus}>Input Focus</button>
+            <button onClick={setClear}>Input Clear</button>
         </div>
     )
 }
