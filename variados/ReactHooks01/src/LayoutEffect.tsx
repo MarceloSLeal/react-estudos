b483b0c7f9375f7e27deac5cb3d854e956453b60
MarceloSLeal <marcelosleal@outlook.com>
2024-04-10 01:36:59 -0300

    Corrigindo alguns erros que ocorreram em runtime no navegador

--- variados/ReactHooks01/src/LayoutEffect.tsx
+++ variados/ReactHooks01/src/LayoutEffect.tsx
@@ -1,20 +1,26 @@
 import { useEffect, useLayoutEffect, useRef } from "react";
 
 const LayoutEffectTutorial = () => {
-    const inputRef = useRef(null);
+    const inputRef = useRef<HTMLInputElement | null>(null);
 
     useLayoutEffect(() => {
-        console.log(inputRef.current.value);
+        if (inputRef.current) {
+            console.log(inputRef.current.defaultValue);
+        } else {
+            console.log("Input Null")
+        }
     }, []);
 
     useEffect(() => {
-        inputRef.current.value = "HELLO";
+        if (inputRef.current) {
+            inputRef.current.defaultValue = "HELLO";
+        }
     }, []);
 
     return (
         <div className="div1">
             <h2>---useLayoutEffect---</h2>
-            <input ref={inputRef} value="PEDRO" style={{ width: 400, height: 30}} />
+            <input ref={inputRef} defaultValue="PEDRO" style={{ width: 400, height: 30}} />
         </div>
     )
     
