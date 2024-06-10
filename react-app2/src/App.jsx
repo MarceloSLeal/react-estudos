946c221572b1215e55608c63161ab0ff30774aa1
MarceloSLeal <marcelosleal@outlook.com>
2024-06-10 06:24:46 -0300

    Tentando montar a barra lateral mas estou com alguns problemas pra resolver, talvez seja por causa de uma versão antiga do npm, eu não sei, preciso investigar

--- react-app2/src/App.jsx
+++ react-app2/src/App.jsx
@@ -1,6 +1,23 @@
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
 import Topbar from './scenes/global/Topbar';
+import Sidebar from './scenes/global/Sidebar';
+import Dashboard from "./scenes/dashboard";
+
+// import Customers from "./scenes/customers";
+// import Employees from "./scenes/employees";
+// import Offices from "./scenes/offices";
+// import OrderDetails from "./scenes/orderdetails";
+// import Orders from "./scenes/orders";
+// import Payments from "./scenes/payments";
+// import ProductLines from "./scenes/productlines";
+// import Products from "./scenes/products";
+//
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
 
 function App() {
     const [theme, colorMode] = useMode();
@@ -10,8 +27,27 @@ function App() {
             <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <div className="app">
+                    <Sidebar />
                     <main className="content">
                         <Topbar />
+                        <Routes>
+                            <Route path="/" element={<Dashboard />} />
+
+                            {/* <Route path="/customers" element={<Customers />} /> */}
+                            {/* <Route path="/employees" element={<Employees />} /> */}
+                            {/* <Route path="/offices" element={<Offices />} /> */}
+                            {/* <Route path="/orderdetails" element={<OrderDetails />} /> */}
+                            {/* <Route path="/orders" element={<Orders />} /> */}
+                            {/* <Route path="/payments" element={<Payments />} /> */}
+                            {/* <Route path="/productlines" element={<ProductLines />} /> */}
+                            {/* <Route path="/products" element={<Products />} /> */}
+                            {/**/}
+                            {/* <Route path="/line" element={<Line />} /> */}
+                            {/* <Route path="/pie" element={<Pie />} /> */}
+                            {/* <Route path="/geography" element={<Geography />} /> */}
+                            {/* <Route path="/calendar" element={<Calendar />} /> */}
+
+                        </Routes>
                     </main>
                 </div>
             </ThemeProvider>
--- react-app2/src/scenes/global/Sidebar.jsx
+++ react-app2/src/scenes/global/Sidebar.jsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+//import 'react-pro-sidebar/dist/css/styles.css';
+import { Box, IconButton, Typography, useTheme } from '@mui/base';
+import { link } from "react-router-dom";
+import { tokens } from "../../theme";
+import LightModeOutLinedIcon from "@mui/icons-material/LightModeOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutLinedIcon from "@mui/icons-material/PeopleOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutLinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutLinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutLinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedicon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutLinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutLinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutLinedIcon from "@mui/icons-material/MapOutlined";
+
 const Sidebar = () => {
-    return <div>Sidebar</div>
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [selected, setSelected] = useState("Dashboard");
+
+    return (
+        <Box
+            sx={{
+                "& .pro-sidebar-inner": {
+                    background: `${colors.primary[400]} !important`,
+                },
+                "& .pro-icon-wrapper": {
+                    backgroundColor: "transparent !important",
+                },
+                "& .pro-inner-item": {
+                    padding: "5px 35px 5px 20px !important",
+                },
+                "& .pro-inner-item:hover": {
+                    color: "#868dfb !important",
+                },
+                "& .pro-menu-item.active": {
+                    color: "#6870fa !important",
+                },
+            }}
+        ></Box>
+    );
 }
 
 export default Sidebar;
